eb2f1f4e90a6b0d21d43d79b4eaaa0bf46c2eb1d
Praveen Banthia <pbanthia@masonlive.gmu.edu>
2017-03-03 03:42:47 -0600

    Menus 1)Added menu res and populated with item 2)Replace default menu with custom menu 3)Added action to menu to change text

--- PluralSight/android-hello-world/app/src/main/java/com/sqisland/android/hello/MainActivity.java
+++ PluralSight/android-hello-world/app/src/main/java/com/sqisland/android/hello/MainActivity.java
@@ -2,11 +2,49 @@ package com.sqisland.android.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+  private TextView greetingView;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    greetingView = (TextView) findViewById(R.id.greeting);
   }
+
+  /**
+   * This is overriding default menu option with custom menu.
+   * Menu inflater takes 2 inputs one the xml containing menu details
+   * and menu object passed in
+   * @param menu: Menu object
+   * @return true
+   */
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+  }
+
+  /**
+   * Here we assign action to menu items that we are showing in
+   * {@code onCreateOptionsMenu}
+   * @param item : Respective Menu Item
+   * @return true if  menu item with action selected else default
+   *
+   */
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if(item.getItemId() == R.id.action_bye){
+      greetingView.setText(R.string.bye);
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+
 }
