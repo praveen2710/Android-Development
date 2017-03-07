ec0128b79fc6ea400af7bef8bab1103693df4c9a
Praveen Banthia <pbanthia@masonlive.gmu.edu>
2017-03-07 02:11:57 -0600

    added multiple functionality to the app

--- PluralSight/android-Intents/PlatformFeatures/README.md
+++ PluralSight/android-Intents/PlatformFeatures/README.md
@@ -2,13 +2,14 @@
 This app will use intents to call into various activities of the platform
 
 ## Features of App
-* Identify when phone is switched on and off
-* Indicate change in battery level of app
-* Schedule app to trigger in 5 minutes
-* Open myLinkedIn Profile from option menu
-* Add menu option to call me 
-* Take a picture from the app
-* Access phone contact from the app
+1. Identify when phone is plugged in and plugged out
+2. Indicate battery status of app
+3. Schedule app to trigger in 5 minutes
+4. Open myLinkedIn Profile from option menu
+5. Add menu option to call me on my cellPhone 
+6. Take a picture from the app
+7. Access phone contact from the app
 
 ##Development iterations
-*Initial commit and readme listing features of the app.
+* Initial commit and readme listing features of the app.
+* Added feature's 1,2,3,5 to the app
--- PluralSight/android-Intents/PlatformFeatures/app/src/main/java/com/wordpress/bebetteratandroid/platformfeatures/MainActivity.java
+++ PluralSight/android-Intents/PlatformFeatures/app/src/main/java/com/wordpress/bebetteratandroid/platformfeatures/MainActivity.java
@@ -1,7 +1,17 @@
 package com.wordpress.bebetteratandroid.platformfeatures;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,72 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuOptionCheckBatteryStatus:
+                checkBatteryStatus();
+                break;
+            case R.id.menuQuit:
+                finish();
+                break;
+            case R.id.menuShowActivity:
+                scheduleActivity();
+                break;
+            case R.id.menuViewLinkedIn:
+                viewMyProfile();
+                break;
+            case R.id.menuCallMe:
+                contactMe();
+                break;
+            default:
+                super.onOptionsItemSelected(item);
+                break;
+        }
+        return true;
+    }
+
+    private void contactMe() {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:2022976795"));
+        startActivity(intent);
+    }
+
+    /**
+     * Calling inbuild service to view a web page
+     */
+    private void viewMyProfile() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/praveenbanthia"));
+        startActivity(intent);
+    }
+
+    /**
+     * This method is to show how we can start an activity with 5 sec delay using system service
+     */
+    private void scheduleActivity() {
+        Intent intent = new Intent("com.wordpress.bebetteratandroid.platformfeatures.DELAYED_TIMER");
+        intent.putExtra("Name", "Praveen");
+        PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        long alarmTime = SystemClock.elapsedRealtime() + 5000;
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME, alarmTime, pi);
+    }
+
+    /**
+     * This method is to register actions that cannot be registered at compile time
+     */
+    private void checkBatteryStatus() {
+        BatteryStatusReceiver b1 = new BatteryStatusReceiver();
+        this.registerReceiver(b1,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
 }
